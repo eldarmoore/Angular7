29fa82c710794628d10f6b0777f6d2e85add9097
Eldar <eldar.moore@gmail.com>
2018-12-27 23:12:45 +0200

    Many cool changes 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,29 +6,32 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  friendlyMaxAttack = 166;
-  friendlyMinAttack = 110;
+  friendlyMaxAttack = 24;
+  friendlyMinAttack = 16;
   friendlyCrit = 10;
-  friendlyHealth = 3980;
-  friendlyArmor = 24;
+  friendlyHealth = 300;
+  friendlyArmor = 286;
+  friendlyBlockChance = 33;
+  friendlyBlock = 96;
   friendlyCurrentHealth: number;
   friendlyHealthPercentage: number;
   friendlyCritMultiplier = 4;
-  enemyMaxAttack = 166;
-  enemyMinAttack = 110;
+  friendlyFilteredAttack: number;
+  enemyMaxAttack = 14;
+  enemyMinAttack = 8;
   enemyCrit = 10;
-  enemyHealth = 3980;
-  enemyArmor = 34;
+  enemyHealth = 300;
+  enemyArmor = 96;
+  enemyBlockChance = 10;
+  enemyBlock = 30;
   enemyCurrentHealth: number;
-  enemyHealthPercentage: number ;
+  enemyHealthPercentage: number;
   enemyCritMultiplier = 4;
+  enemyFilteredAttack: number;
   status: string;
   friendlyCombatLog = [];
   enemyCombatLog = [];
-  CombatLog = [
-    ['friendly'],
-    ['enemy']
-  ];
+  combatLog: number[][] = new Array<number[]>();
 
   resetStats() {
     this.friendlyCurrentHealth = this.friendlyHealth;
@@ -49,7 +52,7 @@ export class AppComponent {
   progressBar(value) {
     if (value > 60) {
       return 'progress-bar-success';
-    } else if (value > 40) {
+    } else if (value > 20) {
       return 'progress-bar-warning';
     } else {
       return 'progress-bar-danger';
@@ -64,12 +67,23 @@ export class AppComponent {
     }
   }
 
-  armorMitigation(armor) {
-    return armor / 100;
+  blockChance(chance, damage, blockValue) {
+    if (Math.random() * 100 < chance) {
+      if (damage < blockValue) {
+        return 0;
+      } else {
+        return damage - blockValue;
+      }
+    } else {
+      return damage;
+    }
+  }
+
+  armorMitigation(damage, armor) {
+    return Math.round((1 - (armor / 1000)) * damage);
   }
 
   onFight() {
-
     if (this.friendlyCurrentHealth && this.enemyCurrentHealth > 0) {
 
       // Generate random number between min - max
@@ -79,11 +93,13 @@ export class AppComponent {
       // Critical strike chance
       friendlyAttack = this.critChance(this.friendlyCrit, friendlyAttack, this.friendlyCritMultiplier);
       if (this.enemyCurrentHealth - friendlyAttack > 0) {
-        this.enemyCurrentHealth -= friendlyAttack;
-        this.friendlyCombatLog.push(friendlyAttack);
+        this.friendlyFilteredAttack = this.blockChance(this.enemyBlockChance, this.armorMitigation(friendlyAttack, this.enemyArmor), this.enemyBlock);
+        console.log('Friendly Attack: ' + friendlyAttack + ' after mitigation ' + this.friendlyFilteredAttack);
+        this.enemyCurrentHealth -= this.friendlyFilteredAttack;
+        this.friendlyCombatLog.push(this.friendlyFilteredAttack);
       } else {
         this.enemyCurrentHealth = 0;
-        this.friendlyCombatLog.push(friendlyAttack);
+        this.friendlyCombatLog.push(this.friendlyFilteredAttack);
         this.status = 'Friendly player has WON!';
       }
 
@@ -94,12 +110,13 @@ export class AppComponent {
       // Critical strike chance
       enemyAttack = this.critChance(this.enemyCrit, enemyAttack, this.enemyCritMultiplier);
       if (this.friendlyCurrentHealth - enemyAttack > 0) {
-        this.friendlyCurrentHealth -= enemyAttack;
-        this.enemyCombatLog.push(enemyAttack);
-        // this.CombatLog['enemy'].push(enemyAttack);
+        this.enemyFilteredAttack = this.blockChance(this.friendlyBlockChance, this.armorMitigation(enemyAttack, this.friendlyArmor), this.friendlyBlock);
+        console.log('Enemy Attack: ' + enemyAttack + ' after mitigation ' + this.enemyFilteredAttack);
+        this.friendlyCurrentHealth -= this.enemyFilteredAttack;
+        this.enemyCombatLog.push(this.enemyFilteredAttack);
       } else {
         this.friendlyCurrentHealth = 0;
-        this.enemyCombatLog.push(enemyAttack);
+        this.enemyCombatLog.push(this.enemyFilteredAttack);
         this.status = 'Enemy player has WON!';
       }
     }
